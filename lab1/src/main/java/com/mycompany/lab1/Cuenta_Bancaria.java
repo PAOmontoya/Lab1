e9a6cf3086efd53d628c1a38e3ce063c902f2a50
PAOmontoya <123204626+PAOmontoya@users.noreply.github.com>
2023-10-13 15:49:21 -0600

    Add files via upload

---
+++ lab1/src/main/java/com/mycompany/lab1/Cuenta_Bancaria.java
@@ -0,0 +1,35 @@
+package com.mycompany.lab1;
+
+public class Cuenta_Bancaria {
+    private int codigo;
+    private String nombre;
+    private double saldo;
+    
+    public Cuenta_Bancaria(int codigo, String nombre){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        saldo=500;
+    }
+    public void depositar(double monto){
+        if(monto>0){
+            saldo+=monto;
+        }
+    }
+    public boolean retirar(double monto){
+        if(monto>0&&monto<=saldo){
+            saldo-=monto;
+            return true;
+        }
+        return false;
+    }
+    public void print(){
+        System.out.println("Codigo: "+codigo+"\nNombre: "+nombre+"\nSaldo: "+saldo);
+    }
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public double getSaldo(){
+        return saldo;
+    }
+}
---
+++ lab1/src/main/java/com/mycompany/lab1/Globals.java
@@ -0,0 +1,7 @@
+package com.mycompany.lab1;
+import java.util.ArrayList;
+
+public class Globals {
+    public static ArrayList<Cuenta_Bancaria> cuentas= new ArrayList<Cuenta_Bancaria>();   
+ 
+}
